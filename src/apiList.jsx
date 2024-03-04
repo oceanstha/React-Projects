5018c52702faae0779b6ad2612ca01674a2ed9b7
sthaocean <socean@insightworkshop.io>
2024-03-04 09:48:57 +0545

    update: netflix anime app first upload

---
+++ src/apiList.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+export const searchTitle = async (page) => {
+
+  const url = `https://api.jikan.moe/v4/top/anime?page=${page}`;
+  const options = {
+    method: 'GET',
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    console.log("result at api list", result);
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+
+export const allAnime=async()=>{
+
+  const url="https://api.jikan.moe/v4/anime";
+  const options={
+    method:'GET'
+  }
+
+  try{
+    const response=await  fetch(url,options);
+    const result=response.json();
+    return result;
+  }catch(error){
+    console.error("Error while fetching anime data");
+  }
+}
+
+
+
---
+++ src/pages/Home.jsx
@@ -0,0 +1,116 @@
+import React, { useEffect, useState } from "react";
+import { searchTitle, allAnime } from "../apiList";
+import { Card, CardContent, CardDescription } from "@/components/ui/card";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu"
+
+const Home = () => {
+  const [allSearchData, setAllSearchData] = useState([]);
+  const [allAnimeData, setAllAnimeData] = useState([]);
+
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const fetchSearchAll = async (page) => {
+    const data = await searchTitle(page);
+    setAllSearchData(data);
+  };
+
+  const fetchAllAnime = async () => {
+    const data = await allAnime();
+    setAllAnimeData(data);
+  }
+
+  useEffect(() => {
+    fetchSearchAll(currentPage);
+  }, [currentPage]);
+
+  useEffect(() => {
+    fetchAllAnime();
+  }, [])
+
+
+
+  return (
+    <div>
+      <div className='bg-[#040f13] h-[50px]'>
+        <NavigationMenu >
+          <NavigationMenuList>
+          <NavigationMenuItem>
+          <img className=' mr-[10px] h-[50px] w-[25px]' src="src/assets/logo-transparent.png"/>
+          </NavigationMenuItem>
+            <NavigationMenuItem className='p-[12px] font-["Times"] hover:text-white hover:cursor-pointer text-gray-400 text-[16px]'>Top Anime</NavigationMenuItem>
+            <NavigationMenuItem>Ocean</NavigationMenuItem>
+            </NavigationMenuList>
+        </NavigationMenu>
+      </div>
+      <div className="bg-hero-pattern bg-center bg-cover bg-fixed flex justify-center items-center	pt-[20px]">
+        <div className="h-[75%] w-[75%] backdrop-blur-sm">
+          <div className="flex flex-wrap justify-content justify-center pt-[25px] ">
+            {allSearchData.data?.map((item, index) => {
+              return (
+                <Card className="w-[220px] h-[333px] mx-4 mb-10 bg-transparent border-none opacity-85 hover:opacity-100 hover:cursor-pointer">
+                  <CardDescription key={index} className="p-0 rounded-md">
+                    {" "}
+                    <img
+                      src={item.images?.jpg?.image_url}
+                      className="w-[220px] h-[267px] rounded-t-md"
+                    />
+                  </CardDescription>
+                  <CardContent className="text-left truncate pl-2 mb-[-25px] text-lg text-white ">
+                    {item.title}
+                  </CardContent>
+                  <CardContent className="text-left truncate pl-2 text-sm text-white">
+                    {item?.genres?.map((item, index) => {
+                      return <span key={index}>{item?.name}-</span>;
+                    })}
+                  </CardContent>
+                </Card>
+              );
+            })}
+          </div>
+          <div className="flex flex-wrap justify-content justify-center mt-[50px] ">
+            <Pagination>
+              <PaginationContent>
+                <PaginationItem>
+                  <PaginationPrevious
+                    className="text-white"
+                    href="#"
+                    onClick={() => setCurrentPage(currentPage - 1)}
+                  />
+                </PaginationItem>
+                <PaginationItem>
+                  <PaginationLink className="text-white" href="#">
+                    {currentPage}
+                  </PaginationLink>
+                </PaginationItem>
+                <PaginationItem>
+                  <PaginationNext
+                    className="text-white"
+                    onClick={() => setCurrentPage(currentPage + 1)}
+                  />
+                </PaginationItem>
+              </PaginationContent>
+            </Pagination>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
